4330ea80c5a2ac3f6d9b3184fe9a71376834a40d
jensii3 <jw.98@web.de>
2024-02-03 12:54:55 +0100

    refactor

--- src/main/java/de/sopra/echoserver/SimpleEchoServer.java
+++ src/main/java/de/sopra/echoserver/SimpleEchoServer.java
@@ -10,18 +10,22 @@ public class SimpleEchoServer
         try {
             System.out.println("Warte auf Verbindung auf Port 6000...");
             ServerSocket echod = new ServerSocket(6000);
-            Socket socket = echod.accept();
-            System.out.println("Verbindung hergestellt");
-            InputStream in = socket.getInputStream();
-            OutputStream out = socket.getOutputStream();
-            int c;
-            while ((c = in.read()) != -1) {
-                out.write((char)c);
-                System.out.print((char)c);
+
+            while (true) {
+                Socket socket = echod.accept();
+                System.out.println("Verbindung hergestellt");
+                InputStream in = socket.getInputStream();
+                OutputStream out = socket.getOutputStream();
+                int c;
+                while ((c = in.read()) != -1) {
+                    out.write((char)c);
+                    System.out.print((char)c);
+                }
+            System.out.println("Verbindung beenden");
+            socket.close();
+            echod.close();
             }
-//            System.out.println("Verbindung beenden");
-//            socket.close();
-//            echod.close();
+
         } catch (IOException e) {
             System.err.println(e.toString());
             System.exit(1);
